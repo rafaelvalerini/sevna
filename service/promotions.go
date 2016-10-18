08cf3971c397b8e1077d6944e24fde888ff46079
rafaelvalerini <rafael.valerini@luizalabs.com>
2016-10-17 22:41:11 -0200

    cabify 15 percent, max 15$

--- service/promotions.go
+++ service/promotions.go
@@ -3,9 +3,6 @@ package service
 import (
 	"agregador/model"
 	"agregador/repository"
-	"strings"
-	"strconv"
-	"fmt"
 )
 
 func AddPromotions(players []model.Player) {
@@ -22,27 +19,9 @@ func AddPromotions(players []model.Player) {
 
  					if promotion.Modality == players[idx].Modality.Name{
 
- 						fmt.Println(players[idx].Modality.Name)
+						players[idx].Modality.Promotion = promotion
 
- 						if promotion.Id == "3"{
-
- 							value,_ := strconv.ParseFloat(strings.Replace(strings.Replace(strings.Replace(strings.Replace(players[idx].Price, ",", ".", -1), " ", "", -1), "R$", "", -1), "-", "", -1), 64)
-
- 							if value <= 15.0{
-
- 								players[idx].Modality.Promotion = promotion
-
- 								continue playerfor
-
- 							}
-
- 						}else{
-
- 							players[idx].Modality.Promotion = promotion
-
- 							continue playerfor
-
- 						}
+						continue playerfor
 
  					}
 
